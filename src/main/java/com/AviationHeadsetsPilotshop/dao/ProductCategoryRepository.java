c8283046ce4a8a7e309b6ee624d28f3f743ec711
DominikBielecki <bielecki.dominik@gmail.com>
2020-09-15 15:58:36 +0200

    Adding CrossOrigin to ProductCategoryRepository and ProductRepository

--- src/main/java/com/AviationHeadsetsPilotshop/dao/ProductCategoryRepository.java
+++ src/main/java/com/AviationHeadsetsPilotshop/dao/ProductCategoryRepository.java
@@ -3,7 +3,9 @@ package com.AviationHeadsetsPilotshop.dao;
 import com.AviationHeadsetsPilotshop.entity.ProductCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin("http://localhost:4200")
 @RepositoryRestResource(collectionResourceRel = "productCategory", path = "product-category") //changing default settings for name of JSON entry and for ref path
 public interface ProductCategoryRepository extends JpaRepository<ProductCategory, Long> {
 }
