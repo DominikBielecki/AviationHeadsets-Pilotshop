e4a92402638dc11874a10a9f55f114827ed774ab
DominikBielecki <bielecki.dominik@gmail.com>
2020-10-13 20:16:16 +0200

    Adding new Query Method for searching product by category.

--- src/main/java/com/AviationHeadsetsPilotshop/dao/ProductRepository.java
+++ src/main/java/com/AviationHeadsetsPilotshop/dao/ProductRepository.java
@@ -1,9 +1,16 @@
 package com.AviationHeadsetsPilotshop.dao;
 
 import com.AviationHeadsetsPilotshop.entity.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @CrossOrigin("http://localhost:4200")
 public interface ProductRepository extends JpaRepository<Product, Long>{
+
+    Page<Product> findByCategoryId(@RequestParam("id") Long id, Pageable pageable);
+    //Query Method, behind is something similar to this: SELECT * FROM product where category_id=?
+
 }
